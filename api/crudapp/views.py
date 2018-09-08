a184786969d021b8be363d5c92d45e2f477c21b7
MichaelJames02 <michael4achieve@gmail.com>
2018-09-08 10:43:01 -0300

    Created views for sku model, available sku model, and notification model

--- api/crudapp/views.py
+++ api/crudapp/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets, routers
+from . import serializers, models
 
-# Create your views here.
+
+
+# Root API class
+class CrudApiRoot(routers.APIRootView):
+    """
+    This is the ROOT API
+    """
+
+# Documented class
+class DocumentedApiView(routers.DefaultRouter):
+    APIRootView = CrudApiRoot
+
+
+# sku model view class
+class SkuModelView(viewsets.ViewSet):
+    serializer_class = serializers.SkuSerializer
+    # permission_classes = ()
+    queryset = models.SkuModel.objects.all()
+
+
+# available sku model view class
+class AvailableSkuModelView(viewsets.ViewSet):
+    serializer_class = serializers.AvailableSkuSerializer
+    # permission_classes = ()
+    queryset = models.AvailableSkuModel.objects.all()
+
+
+# notification model view class
+class NotificationModelView(viewsets.GenericViewSet):
+    serializer_class = serializers.NotificationSerializer
+    # permission_classes = ()
+    queryset = models.NotificationModel.objects.all()
