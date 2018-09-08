8bc2840f1fb0ea8998a8ea583c5393def34f8839
MichaelJames02 <michael4achieve@gmail.com>
2018-09-08 15:00:16 -0300

    changed GenericViewSet to ModelViewSet

--- api/crudapp/models.py
+++ api/crudapp/models.py
@@ -9,11 +9,18 @@ class SkuModel(models.Model):
     sku_id = models.CharField(max_length=10)
     sent_date = models.DateTimeField()
 
+    def __str__(self, *args, **kwargs):
+        data = [self.product_name, self.product_id]
+        return 'Product name: {}, Product ID: {}'.format(*data)
+
 
 # available sku model class
 class AvailableSkuModel(models.Model):
     available_sku = models.ForeignKey('SkuModel', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self, *args, **kwargs):
+        return '{}, Price: {}'.format(self.available_sku, self.price)
 
 
 # notification model class
