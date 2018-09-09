9dea5763fb8c98ef8d943bb802116f58d20d2d3d
MichaelJames02 <michael4achieve@gmail.com>
2018-09-09 18:59:24 -0300

    created get_notification function in the model.py file

--- api/crudapp/models.py
+++ api/crudapp/models.py
@@ -1,3 +1,4 @@
+import requests
 from django.db import models
 
 
@@ -22,8 +23,36 @@ class AvailableSkuModel(models.Model):
     def __str__(self, *args, **kwargs):
         return '{}, Price: {}'.format(self.available_sku, self.price)
 
+    def get_price(self):
+        if self.price in range(10, 41):
+            return self.price
+
+    def set_price(self, price):
+        self.price = price
+
 
 # notification model class
 # return sku and updated list
 class NotificationModel(models.Model):
     notification = models.ForeignKey('SkuModel', on_delete=models.CASCADE)
+
+    def get_notification():
+        api_url = "https://sandboxmhubapi.epicom.com.br/v1"
+        access_key = "897F8D21A9F5A"
+        token = "Ip15q6u7X15EP22GS36XoNLrX2Jz0vqq"
+        auth = (access_key, token)
+        respones = requests.get(api_url, auth=auth)
+        r = respones.text
+        file = open('/home/michael/Desktop/Bot/epicom-api/crud-api/api/crudapp/templates/notification_file.html', 'w')
+        file.write(r)
+        file.close()
+        return file
+
+    def update_sku(self):
+        api_url = "https://sandboxmhubapi.epicom.com.br/v1"
+        data = ""
+        access_key = "897F8D21A9F5A"
+        token = "Ip15q6u7X15EP22GS36XoNLrX2Jz0vqq"
+        auth = (access_key, token)
+        respones = requests.post(api_url, auth, data)
+        return  respones
